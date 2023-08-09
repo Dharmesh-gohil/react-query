abf4f836f3dd61a21a2995c78733e7675a960879
Dharmesh-gohil <dharmesh.vatsalgohil@gmail.com>
2023-08-09 18:24:27 +0530

    mostly done refactor remaining

--- starter/src/Form.jsx
+++ starter/src/Form.jsx
@@ -1,10 +1,29 @@
+import { useMutation,useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
+import customFetch from "./utils"
+import { toast} from "react-toastify"
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
+const queryClient=useQueryClient()
+  // const result = useMutation({
+  const { mutate:createTask,isLoading} = useMutation({
+
+    mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onSuccess: () => { 
+      queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+        toast.success("item added successfully to the list"),
+        setNewItemName("")
+    },
+    // onError: (error) => {console.log(error) }
+    onError: (error) => { toast.error(error.response.data.msg)}
+    })
+    // console.log(result)
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    createTask(newItemName)
+    
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -16,7 +35,10 @@ const Form = () => {
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn'>
+        <button type='submit' className='btn' disabled={isLoading}>
+          {/* here we use disabled property of button bcoz wheb we 
+          //loading user cant submit another tasks and defalult property 
+          //of button is false */}
           add task
         </button>
       </div>
--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,8 +1,39 @@
 import SingleItem from './SingleItem';
+import { useQuery } from '@tanstack/react-query';
+import customFetch from "./utils"
+
 const Items = ({ items }) => {
+  //here axios return promise but we use customFetch so it also return promise coz it also contain axios
+  // const result = useQuery({
+  const { isLoading,data,isError,error}=useQuery({
+    queryKey: ["tasks"],
+    // queryFn: () => customFetch.get("/")
+    //this will return data.data so we can use another way like to destructer it 
+    //so we have to loop only data.taskList only not data.data.taskList
+    queryFn: async () => {
+      const {data} = await customFetch.get("/")
+    return data
+    }
+  })
+  // console.log(result)
+  // console.log(data)
+  if (isLoading) { 
+    return <p style={{marginTop:"2rem "}}> Loading....</p>
+  }
+
+  if (isError) { 
+    return <p style={{marginTop:"2rem "}}> There is an error ....</p>
+  }
+// console.log(error)
+//  if (error) { 
+//    return <p style={{ marginTop: "2rem " }}> { error.message}{ error.response.data}</p>
+//   }
+
   return (
     <div className='items'>
-      {items.map((item) => {
+      {/* {data.data.taskList.map((item) => { */}
+      {data.taskList.map((item) => {
+    
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
